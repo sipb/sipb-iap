a21d09c57714863d75e884f5f35ae621df2ee9ff
Andrew M. Farrell <amfarrell@mit.edu>
2013-07-30 01:40:57 -0400

    add files from ~/web_scripts which are now symlinked to there.

---
+++ debugutil.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import os, time, sys, socket, sys, traceback
+try:
+    import thread
+except ImportError:
+    pass
+import threading
+
+(SPEW,DEBUG,INFO,WARN,ERROR,FATAL) = (0,1,2,3,4,5)
+
+debug_level=SPEW
+
+def zwrite(msg,urgency=DEBUG):
+    """use this for printline debugging"""
+    if urgency < debug_level:
+        return
+    user = "-c sipb-iap-dbg"
+    try:
+        os.system("zwrite -d %s -m '%s'" % (user,msg))
+    except Exception,e:
+        try: 
+            os.system("zwrite -d -c afarrell-dbg -i sipb-iap -m 'ZWRITE ERROR FROM SIPB_IAP %s'" % user)
+            os.system("zwrite -d -c afarrell-dbg -i sipb-iap -m '%s'" % str(sys.exc_info()))
+            os.system("zwrite -d -c afarrell-dbg -i sipb-iap -m '%s'" % str(e))
+            os.system("zwrite -d -c afarrell-dbg -i sipb-iap -m 'real message \n%s'" % msg)
+        except:
+            pass
+def ztraceback():
+    zwrite(traceback.format_exc())
